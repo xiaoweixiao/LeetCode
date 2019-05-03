59f36f2f1feb2dbfebc28e60bc9096d9fc1f031f
Sunshine boy <43498959+xiaoweixiao@users.noreply.github.com>
2019-05-03 21:14:28 +0800

    Create 4.寻找两个排序数组的中位数.cpp

---
+++ 4.寻找两个排序数组的中位数.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size()+nums2.size();
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
+        if(n%2==0)
+            return (nums1[n/2]+nums1[n/2-1])/2.0;
+        else
+            return nums1[n/2];
+    }
+};
