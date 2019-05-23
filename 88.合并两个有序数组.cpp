6c78cd73e8e4afa7b4e4320ed04214388ad8afeb
Sunshine boy <43498959+xiaoweixiao@users.noreply.github.com>
2019-05-23 23:27:18 +0800

    Update 88.合并两个有序数组.cpp

--- 88.合并两个有序数组.cpp
+++ 88.合并两个有序数组.cpp
@@ -12,19 +12,19 @@ public:
 class Solution {
 public:
 	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		int sum = m + n;
-		int pos = 0;//指向nums2准备存入的nums1的元素下标
+		int i = 0;//标记nums2将要插入的数的下标
 		if (n>0)
 		{
-			for (int i = 0; i<sum&&n; i++)
+			for (int pos = 0; (pos<n + m&&n>0); pos++)//pos表示nums2[i]将要被插入的坑位
 			{
-				if (nums2[pos] <= nums1[i] || i >= m)
+				if (nums2[i]<nums1[pos]||pos>=m)//我比你还矮，往后站,pos都到了队尾了
 				{
-					for (int k = nums1.size() - 1; k>i; k--)//将元素后移一位
-						nums1[k] = nums1[k - 1];
-					nums1[i] = nums2[pos];//插入元素
-					pos++,n--,m++;
+					for (int j = m-1; j >=pos; j--)
+						nums1[j+1] = nums1[j];
+					nums1[pos] = nums2[i];
+					i++, n--, m++;
 				}
+				
 			}
 		}
 	}
