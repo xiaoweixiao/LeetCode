9240b499bbbe74b9f98f734cb6368bb3bcfc0881
Sunshine boy <43498959+xiaoweixiao@users.noreply.github.com>
2019-06-02 12:21:21 +0800

    Update 3无重复字符的最长子串.cpp

--- 3无重复字符的最长子串.cpp
+++ 3无重复字符的最长子串.cpp
@@ -1,16 +1,25 @@
+//暴力破解时间复杂度为O(n*n*n)，超时，代码不写了。
+
+//滑动窗口，时间复杂度O(n*n)
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
 		int  size = s.size();
 		int i = 0, ret = 0;
-		for (int j = 0; j<size; j++){//[i,j]是不重复子串区间，j每向后移动一位，判断一次
-			for (int k = i; k<j; k++)
-			if (s[k] == s[j]){
-				i = k + 1;
-				break;
-			}
-			ret =max(j - i + 1,ret);
+		for (int j = 0; j<size; j++)//[i,j]是不重复子串区间，j每向后移动一位，判断一次
+		{
+			for (int k = i; k < j; k++)
+			{
+				if (s[k] == s[j])
+				{
+					i = k + 1;
+					break;
+				}
+            }
+			ret = max(j - i + 1, ret);
 		}
 		return ret;
 	}
 };
+
+//
